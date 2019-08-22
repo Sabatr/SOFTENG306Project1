53a6963fef3535d23ccf511f74b49020c47df5a2
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2019-08-22 16:30:52 +1200

    Added AStar Parallel class

---
+++ src/main/java/algorithm/AStarParallel.java
@@ -0,0 +1,117 @@
+package algorithm;
+
+import graph.Graph;
+import scheduler.AStarComparator;
+import scheduler.State;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+/**
+ * Algorithm which deals with using the A star implementation. Here, a priority queue
+ * is used to ensure that nodes with least cost are placed with greatest priority followed
+ * by their level.
+ */
+public class AStarParallel  implements  Algorithm{
+    private int minFullPath = Integer.MAX_VALUE;
+    private boolean traversed;
+    private PriorityQueue<State> candidate;
+    private HashSet<State> visited;
+    private Graph graph;
+    private State result;
+    private final int MAX_THREADS = 4;
+    private int currentThreads;
+
+    private int num;
+
+    public AStarParallel(int numProcessors, Graph graph) {
+        candidate = new PriorityQueue<>(new AStarComparator());
+        visited = new HashSet();
+        this.graph = graph;
+        traversed = false;
+        candidate.add(new State(numProcessors, graph));
+        currentThreads = 1;
+        num = 0;
+    }
+
+    private synchronized void changeCurrentThreads(int i){
+        currentThreads = currentThreads + i;
+    }
+
+    /**
+     * Runs the algorithm
+     * @return
+     */
+    public State runAlgorithm() {
+        while (!candidate.isEmpty() && candidate.peek().getCostToBottomLevel() <= minFullPath) {
+            if (currentThreads < MAX_THREADS) {
+                changeCurrentThreads(1);
+                new AStarThread().start();
+                iterate();
+            } else {
+                iterate();
+            }
+
+        }
+        return result;
+    }
+
+    private synchronized State pollQueue(){
+        scheduler.State s = candidate.poll();
+        return s;
+    }
+
+    private synchronized void addToQueue(State s1){
+        candidate.add(s1);
+    }
+
+    private void iterate(){
+        scheduler.State s = pollQueue();
+        if (s!=null) {
+            for (scheduler.State s1 : s.generatePossibilities()) {
+                if (!visited.contains(s1)) {
+                    if (s1.getCostToBottomLevel() < minFullPath) {
+                        addToQueue(s1);
+                        if (s1.allVisited() && s1.getCostToBottomLevel() < minFullPath) {
+                            minFullPath = s1.getCostToBottomLevel();
+                            result = s1;
+                        }
+                    }
+                    visited.add(s1);
+                }
+            }
+        }
+    }
+
+    private class AStarThread extends Thread{
+        @Override
+        public void run() {
+            runAlgorithm();
+            changeCurrentThreads(-1);
+        }
+
+
+    }
+
+
+    //Todo implement this class.
+    /*
+    Initialise MinFullPath to integer.Maxint
+    Add the initial State(Empty, VisitedList(root),CandidateList(roots' children),currentCost) to the
+    Priority Queue
+    While the priorityQueue is not empty:
+        Generate the possibilities involving all nodes in the candidate list
+        If we have traversed all nodes and cost is less than the minFullPathCost:
+            Add the possibilities onto the priority queue
+        Else:
+            Add the possibilities onto the priority queue
+        Pop off the priority queue
+        if current is full state and cheaper than minFullPath:
+            replace minFullPath
+            For all states in priority queue:
+                If cost is less than the minFullPathCost:
+                    Remove it from the priority queue
+    done
+    Select the State with cheapest DFS cost
+     */
+}
